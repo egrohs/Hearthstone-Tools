ae767057b361d98793de5c38bca7cc8da737b105
Egrohs <estivador@gmail.com>
2017-04-16 11:28:22 -0300

    Mostra lista.

--- src/hcs/App.java
+++ src/hcs/App.java
@@ -2,6 +2,8 @@ package hcs;
 
 import java.util.Set;
 
+import com.sun.javafx.collections.ImmutableObservableList;
+
 import hcs.Carta.CLASS;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -9,10 +11,16 @@ import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -26,22 +34,54 @@ public class App extends Application {
 		stage.initStyle(StageStyle.TRANSPARENT);
 		// Text text = new Text("Transparent!");
 		// text.setFont(new Font(40));
-		Scene scene = monta();
-		// VBox box = new VBox();
-		// box.getChildren().add(new ImageView(new
-		// Image("file:Bars/AT_001.png")));
-		// box.getChildren().add(new ImageView(new
-		// Image("file:Bars/AT_002.png")));
-		// box.getChildren().add(text);
-		// final Scene scene = new Scene(box, 300, 250);
-		
+		VBox box = new VBox();
+		final TextField tf = new TextField();
+		tf.setMaxSize(130, 20);
+		box.getChildren().add(tf);
+		// RadioButton rb = new RadioButton("WAR");
+		// box.getChildren().add(rb);
+		// RadioButton rb1 = new RadioButton("HUN");
+		// box.getChildren().add(rb1);
+		Spinner<Integer> ss = new Spinner<Integer>(1, 10, 1);
+		box.getChildren().add(ss);
+		ComboBox<CLASS> cb = new ComboBox<CLASS>(new ImmutableObservableList<CLASS>(CLASS.values()));
+		box.getChildren().add(cb);
+		Button b = new Button("GO");
+		box.getChildren().add(b);
+		VBox cartas = new VBox();
+		box.getChildren().add(cartas);
+		b.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				cartas.getChildren().removeAll(cartas.getChildren());
+				Set<Sinergia> sub = Jogos.provaveis(Universo.getCard(tf.getText().toLowerCase()), ss.getValue(),
+						cb.getValue());
+				int cont = (int) Screen.getPrimary().getVisualBounds().getHeight() / 34 - 2;
+				for (Sinergia sinergia : sub) {
+					StackPane stackPane = new StackPane();
+					Carta c = (Carta) sinergia.e2;
+					stackPane.getChildren().add(new ImageView(new Image("file:Bars/" + c.id + ".png")));
+					Text t = new Text(sinergia.valor.toString());
+					t.setFill(Color.WHITE);
+					t.setFont(new Font(20));
+					stackPane.getChildren().add(t);
+					cartas.getChildren().add(stackPane);
+					cont--;
+					if (cont == 0) {
+						break;
+					}
+				}
+			}
+		});
+		Scene scene = new Scene(box);
+		scene.setFill(null);
+		stage.setScene(scene);
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 		// set Stage boundaries to visible bounds of the main screen
 		stage.setX(primaryScreenBounds.getMinX());
 		stage.setY(primaryScreenBounds.getMinY());
-		stage.setWidth(300);
+		stage.setWidth(130);
 		stage.setHeight(primaryScreenBounds.getHeight());
-		stage.setScene(scene);
 		stage.setAlwaysOnTop(true);
 		stage.show();
 	}
@@ -50,30 +90,5 @@ public class App extends Application {
 		Universo.leCards();
 		Jogos.leSinergias();
 		launch(args);
-	}
-
-	private static Scene monta() {
-		VBox box = new VBox();
-		final TextField tf = new TextField();
-		box.getChildren().add(tf);
-		Button b = new Button("GO");
-		box.getChildren().add(b);
-		b.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				Set<Sinergia> sub = Jogos.provaveis(Universo.getCard(tf.getText().toLowerCase()), 1,CLASS.WARRIOR);
-				int cont = (int) Screen.getPrimary().getVisualBounds().getHeight() / 34 - 2;
-				for (Sinergia sinergia : sub) {
-					box.getChildren().add(new ImageView(new Image("file:Bars/" + ((Carta) sinergia.e2).id + ".png")));
-					cont--;
-					if (cont == 0) {
-						break;
-					}
-				}
-			}
-		});
-		Scene sc = new Scene(box);
-		sc.setFill(null);
-		return sc;
 	}
 }
