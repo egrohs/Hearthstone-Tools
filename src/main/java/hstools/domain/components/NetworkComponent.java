a9f68687bda3d58562671bfd116a4ee86b8087e2
Emanuel Motta Grohs <emanuel.grohs@serpro.gov.br>
2024-05-08 13:49:49 -0300

    battlenet api

--- src/main/java/hstools/domain/components/NetworkComponent.java
+++ src/main/java/hstools/domain/components/NetworkComponent.java
@@ -1,8 +1,12 @@
 package hstools.domain.components;
 
+import java.util.Arrays;
+import java.util.Base64;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -17,9 +21,32 @@ public class NetworkComponent {
 	HttpHeaders headers = new HttpHeaders();
 	HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
+	String bNETClientID = "121e0dd9cccb413da3924481f079418d";
+	String bNETClientSecret = "Pxw7S3pT1I2DIev2gdEFTdK0vYAaMXv1";
+	// curl -u {client_id}:{client_secret} -d grant_type=client_credentials https://oauth.battle.net/token
+	String bNEToauthURL = "https://oauth.battle.net/token";
+	
+	String api = "https://us.api.blizzard.com/hearthstone/cards/678?locale=en_US";
+
 	public NetworkComponent() {
 		headers.set("X-RapidAPI-Key", "MpVxk1YV1GmshautO7ajo2MGPknxp1Y1ovXjsnsfl9vhykCnsf");
 		headers.set("X-RapidAPI-Host", "omgvamp-hearthstone-v1.p.rapidapi.com");
+	
+		String encoding = Base64.getEncoder().encodeToString((bNETClientID + ":" + bNETClientSecret).getBytes());
+		headers.set("Authorization", "Basic " + encoding);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+	}
+
+	public static void main(String[] args) {
+		NetworkComponent nc = new NetworkComponent();
+		nc.bNEToauthURL();
+	}
+
+	private void bNEToauthURL() {
+	    HttpEntity<String> requestEntity2 = new HttpEntity<>("grant_type=client_credentials", headers);
+		ResponseEntity<String> response = rest.postForEntity(bNEToauthURL, requestEntity2, String.class);
+		System.out.println(response);
 	}
 
 	public String rapidApiAllCollectibleCards() {
@@ -32,7 +59,6 @@ public class NetworkComponent {
 	}
 
 	public RapidApiInfo rapidApiInfo() {
-		// HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 		ResponseEntity<RapidApiInfo> response = rest.exchange("https://omgvamp-hearthstone-v1.p.rapidapi.com/info",
 				HttpMethod.GET, requestEntity, RapidApiInfo.class, (Object) null);
 		return response.getBody();
